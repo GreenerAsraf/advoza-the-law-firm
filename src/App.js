d9a36cc815a93094c95f40f067e82fd21faacb66
Asraf407 <Asraf.uddin4407@gmail.com>
2022-11-08 00:59:53 +0600

    router implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <h1>welcome</h1>
+           <div className='max-w-screen-xl mx-auto'>
+              <RouterProvider router={router}></RouterProvider>
+          </div>
     </div>
   );
 }
